c9b5db8c717d7d21d2ebcc07ec7a5fc6de3a799e
Kurt212 <kurtis212@gmail.com>
2016-11-15 01:03:20 +0300

    @KSS deleted

--- @KSS/mod.cpp
+++
@@ -1,7 +0,0 @@
-name = "Kurt's Survival System";
-picture = "icon.paa";
-actionName = "Website";
-action = "http://red-bear.ru";
-tooltip = "Kurt's Survival System";
-overview = "Система голода и жажды от Kurt'а";
-
